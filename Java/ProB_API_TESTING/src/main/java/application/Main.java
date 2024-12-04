42c97ed658a75f14dc035736aba049ab81c8da86
Alexander Onofrei <alexonofrei@hotmail.com>
2024-12-04 10:00:44 -0500

    Removed small thing in Main

--- Java/ProB_API_TESTING/src/main/java/application/Main.java
+++ Java/ProB_API_TESTING/src/main/java/application/Main.java
@@ -5,7 +5,6 @@ import application.system_under_test.tls_system_under_test.TLSSystemUnderTest;
 import application.test_examiner.TestExaminer;
 
 public class Main {
-    private static SystemUnderTest tlsSystemUnderTest = new TLSSystemUnderTest();
     private static TestExaminer tlsTestExaminer = new TestExaminer("tls");
 
     public static void main(String[] args) {
