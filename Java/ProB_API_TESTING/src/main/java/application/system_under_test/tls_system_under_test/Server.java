0342c45ff4f763d56303d132aa5831e547fba668
bere1900 <bere1900@dinf-0054-05b.DInf.fsci.usherbrooke.ca>
2024-12-05 13:50:04 -0500

    Coding specific TLS versions and cipher suites for client and server.

--- Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_system_under_test/Server.java
+++ Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_system_under_test/Server.java
@@ -16,7 +16,7 @@ public class Server {
             char[] password = "test123".toCharArray();
             KeyStore keyStore = KeyStore.getInstance("JKS");
 
-            try (FileInputStream fis = new FileInputStream("/Users/alex/Desktop/School/Masters/Projet de Recherche/Code/ALL CODE/TLSModeling/Java/ProB_API_TESTING/src/main/resources/session/serverkeystore")) {
+            try (FileInputStream fis = new FileInputStream("src/main/resources/session/serverkeystore")) {
                 keyStore.load(fis, password);
             }
 
@@ -34,6 +34,14 @@ public class Server {
             // Create SSL server socket
             try (SSLServerSocket serverSocket = (SSLServerSocket) ssf.createServerSocket(PORT)) {
                 System.out.println("Server started. Waiting for client...");
+
+                // Enable custom cipher suites
+                String[] customCipherSuites = {
+                        "TLS_RSA_WITH_AES_256_CBC_SHA"
+                };
+                serverSocket.setEnabledCipherSuites(customCipherSuites);
+                // Specify allowed TLS versions
+                serverSocket.setEnabledProtocols(new String[]{"TLSv1.3"});
 
                 // Accept incoming connections
                 try (SSLSocket clientSocket = (SSLSocket) serverSocket.accept()) {
