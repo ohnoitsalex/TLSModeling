9cb7e102668609fe5ffbf64df79d43acb3dc31c6
Alexander Onofrei <alexonofrei@hotmail.com>
2024-12-09 14:05:52 -0500

    Support for bouncy castle

---
+++ Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_system_under_test/BCServer.java
@@ -0,0 +1,34 @@
+package application.system_under_test.tls_system_under_test;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+public class BCServer {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, CertificateException, NoSuchProviderException, IOException, KeyStoreException, UnrecoverableKeyException {
+        Security.setProperty("crypto.policy", "unlimited");
+        int maxKeySize = javax.crypto.Cipher.getMaxAllowedKeyLength("AES");
+        System.out.println("Max Key Size for AES : " + maxKeySize);
+
+        Security.addProvider(new BouncyCastleProvider());
+        CertificateFactory certFactory= CertificateFactory
+                .getInstance("X.509", "BC");
+
+        X509Certificate certificate = (X509Certificate) certFactory
+                .generateCertificate(new FileInputStream("Baeldung.cer"));
+
+        char[] keystorePassword = "password".toCharArray();
+        char[] keyPassword = "password".toCharArray();
+
+        KeyStore keystore = KeyStore.getInstance("PKCS12");
+        keystore.load(new FileInputStream("Baeldung.p12"), keystorePassword);
+        PrivateKey key = (PrivateKey) keystore.getKey("baeldung", keyPassword);
+    }
+}
