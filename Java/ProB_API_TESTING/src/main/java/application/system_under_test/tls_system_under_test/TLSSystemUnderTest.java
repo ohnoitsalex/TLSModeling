a16be66185f6001fd38f6d51ea8371173528a742
BigOuden <contact@bigouden.org>
2025-05-28 17:03:17 -0400

    Remove InformationCapture usage and switch to TLS_Attacker

--- Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_system_under_test/TLSSystemUnderTest.java
+++ Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_system_under_test/TLSSystemUnderTest.java
@@ -1,62 +1,68 @@
 package application.system_under_test.tls_system_under_test;
 
 import application.config.Config;
-// import application.information_capture.InformationCapture;
-// import application.information_capture.tls_information_capture.TLSInformationCapture;
 import application.system_under_test.SystemUnderTest;
 
 import java.io.IOException;
 
 public class TLSSystemUnderTest extends SystemUnderTest {
-    private Process serverProcess, clientProcess;
-    // private final InformationCapture informationCapture;
 
-    public TLSSystemUnderTest() {
-        // informationCapture = new TLSInformationCapture();
+
+    /* Mode of the TLS system under test, either "client" or "server".*/
+    private String mode;
+
+    
+    /** Processes for the TLS SUT. */
+    private Process sutProcess;
+
+    public TLSSystemUnderTest(String mode) {
+        this.mode = mode;
     }
 
+    /**
+     * Creates the System Under Test (SUT) by starting the TLS client or server process.
+     * This method initializes the SUT based on the specified mode (client or server).
+     */
     @Override
     public void createSUT() {
         try {
-            this.serverProcess = new ProcessBuilder("java", "-cp", Config.CLASSPATH, Config.SERVERCLASSNAME).inheritIO().start();
-            Thread.sleep(2000);  // Wait for 2 seconds (2000 milliseconds) - In order for the server to start properly
-            this.clientProcess = new ProcessBuilder("java", "-cp", Config.CLASSPATH, Config.CLIENTCLASSNAME).inheritIO().start();
-            this.serverProcess.waitFor();
-            this.clientProcess.waitFor();
+            if ("server".equalsIgnoreCase(mode)){
+                // The SUT is the real server (ex: BouncyCastle)
+                this.sutProcess = new ProcessBuilder("java", "-cp", Config.CLASSPATH, Config.SERVERCLASSNAME)
+                    .inheritIO()
+                    .start();
+                System.out.println("TLS Server starting");
+                Thread.sleep(10000); // Wait for 2 seconds - In order for the server to start properly
+            } else if ("client".equalsIgnoreCase(mode)) {
+                // The SUT is the real client
+                this.sutProcess = new ProcessBuilder("java", "-cp", Config.CLASSPATH, Config.CLIENTCLASSNAME).
+                    inheritIO().
+                    start();
+            } else {
+                throw new IllegalArgumentException("Unknown mode: " + mode);
+            }
+
+            this.sutProcess.waitFor(); // waiting for process to end.
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Starts the System Under Test (SUT) in a separate thread.
+     * This method is called to run the SUT without blocking the main thread.
+     */
     @Override
     public void startSUT() {
-
-        // Create the first Runnable for startCapture
-        Runnable information_capture = new Runnable() {
-            @Override
-            public void run() {
-                // informationCapture.startCapture();
-            }
-        };
-
-        // Create the second Runnable for createSUT
-        Runnable createSystemUnderTest = new Runnable() {
-            @Override
-            public void run() {
-                createSUT(); // Assuming createSUT is a static method or method in the current class
-            }
-        };
-
-        // Create two threads for each task - It takes a bit of time before the .txt file is generated.
-        Thread thread1 = new Thread(information_capture);
-        Thread thread2 = new Thread(createSystemUnderTest);
-
-        // Start the threads
-        thread1.start();
-        thread2.start();
+        new Thread(this::createSUT).start();
     }
 
+    /**
+     * Executes the SUT operation.
+     * This method is called to perform the main operation of the SUT.
+     * In this case, it does nothing as the SUT is already running.
+     */
     @Override
     public void executeSUTOperation() {
 
