7e2799208816b854ce8fe8e65a968bd7fb4116a3
BigOuden <contact@bigouden.org>
2025-05-28 13:40:35 -0400

    Add TlsMessageParser
    Use case : parse SUT message to yaml for the ProbModelChecker

--- Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_attacker/utils/TlsMessageParser.java
+++ Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_attacker/utils/TlsMessageParser.java
@@ -1,5 +1,29 @@
 package application.system_under_test.tls_attacker.utils;
 
+import de.rub.nds.tlsattacker.core.protocol.message.ServerHelloMessage;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Utility class for parsing TLS messages.
+ */
 public class TlsMessageParser {
     
+    /**
+     * Parses a ServerHelloMessage into a map of key-value pairs.
+     * @param message The ServerHelloMessage to parse
+     * @return Map containing the parsed data
+     */
+    public static Map<String, String> parseServerHello(ServerHelloMessage message) {
+        Map<String, String> data = new HashMap<>();
+        
+        // Parse basic fields
+        data.put("protocol_version", message.getProtocolVersion().getValue().toString());
+        data.put("random", message.getRandom().getValue().toString());
+        data.put("session_id", message.getSessionId().getValue().toString());
+        data.put("selected_cipher_suite", message.getSelectedCipherSuite().getValue().toString());
+        data.put("selected_compression_method", message.getSelectedCompressionMethod().getValue().toString());
+        
+        return data;
+    }
 }
