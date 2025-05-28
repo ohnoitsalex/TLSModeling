cefb527c9fc1c0f175c922fe353b6d7db2308b44
BigOuden <contact@bigouden.org>
2025-05-28 14:59:26 -0400

    Add parseClientHello

--- Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_attacker/utils/TlsMessageParser.java
+++ Java/ProB_API_TESTING/src/main/java/application/system_under_test/tls_attacker/utils/TlsMessageParser.java
@@ -1,6 +1,7 @@
 package application.system_under_test.tls_attacker.utils;
 
 import de.rub.nds.tlsattacker.core.protocol.message.ServerHelloMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +24,24 @@ public class TlsMessageParser {
         data.put("session_id", message.getSessionId().getValue().toString());
         data.put("selected_cipher_suite", message.getSelectedCipherSuite().getValue().toString());
         data.put("selected_compression_method", message.getSelectedCompressionMethod().getValue().toString());
+        
+        return data;
+    }
+
+    /**
+     * Parses a ClientHelloMessage into a map of key-value pairs.
+     * @param message The ClientHelloMessage to parse
+     * @return Map containing the parsed data
+     */
+    public static Map<String, String> parseClientHello(ClientHelloMessage message) {
+        Map<String, String> data = new HashMap<>();
+        
+        // Parse basic fields
+        data.put("protocol_version", message.getProtocolVersion().getValue().toString());
+        data.put("random", message.getRandom().getValue().toString());
+        data.put("session_id", message.getSessionId().getValue().toString());
+        data.put("cipher_suites", message.getCipherSuites().getValue().toString());
+        data.put("compression_methods", message.getCompressions().getValue().toString());
         
         return data;
     }
