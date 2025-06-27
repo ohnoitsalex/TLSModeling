3fee7d664055f9dc2ec13b7deece7dd8f1e9fd7c
BigOuden <simon.belier29@gmail.com>
2025-06-27 14:43:41 -0400

    Main remade

--- Java/ProB_API_TESTING/src/main/java/application/Main.java
+++ Java/ProB_API_TESTING/src/main/java/application/Main.java
@@ -1,14 +1,22 @@
 package application;
 
 import application.test_examiner.TestExaminer;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Main {
-    public static void main(String[] args) {
-        // Exemple : on teste un serveur réel avec un client TLS-Attacker
-        String type = "tls";       // Choix du modèle
-        String mode = "server";    // Ce que tu veux tester (client ou serveur)
 
-        TestExaminer examiner = new TestExaminer(type, mode);
+    public static void main(String[] args) {
+        // Example : we test a real server with a TLS-Attacker client
+
+        // Register BouncyCastle provider
+        if (Security.getProvider("BC") == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
+
+        String type = "tls"; // Model choice
+
+        TestExaminer examiner = new TestExaminer(type);
         examiner.runTest();
     }
 }
