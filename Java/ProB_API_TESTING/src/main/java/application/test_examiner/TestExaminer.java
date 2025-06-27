e2d78ddbffc3ddd61675c3a6121126744e01e19b
BigOuden <simon.belier29@gmail.com>
2025-06-27 14:45:13 -0400

    New test examiner
    Use of FakeClient and systemundertest

--- Java/ProB_API_TESTING/src/main/java/application/test_examiner/TestExaminer.java
+++ Java/ProB_API_TESTING/src/main/java/application/test_examiner/TestExaminer.java
@@ -3,18 +3,20 @@ package application.test_examiner;
 import application.config.Config;
 import application.information_handler.AbstractInformationComparator;
 import application.information_handler.InformationConvertertoAbstract;
+import application.model_api.ModelExecuter;
 import application.model_api.ModelLoader;
 import application.system_under_test.SystemUnderTest;
-import application.system_under_test.tls_attacker.TLSAttackerClientRunner;
-import application.system_under_test.tls_attacker.TLSAttackerServerRunner;
+import application.system_under_test.tls_attacker.TLSAttackerFakeClient;
+import application.system_under_test.tls_attacker.TLSAttackerSUTServer;
+import application.system_under_test.tls_attacker.utils.TlsYamlParser;
 import application.system_under_test.tls_system_under_test.TLSSystemUnderTest;
-import de.prob.scripting.Api;
+
+import java.util.Map;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import de.prob.MainModule;
-
-
+import de.prob.scripting.Api;
 
 public class TestExaminer {
 
@@ -23,38 +25,34 @@ public class TestExaminer {
 
     /* System Under Test (SUT) */
     private SystemUnderTest systemUnderTest;
-    private SystemUnderTest oppositeTester;
+    private SystemUnderTest fakeClient;
 
     /* Client or server SUT */
-    private String mode; 
+    private String mode;
 
-    public TestExaminer(String type, String mode) {
-        this.mode = mode;
+    public TestExaminer(String type) {
 
         // Initialize Guice injector to load the ProB API
         Injector injector = Guice.createInjector(new MainModule());
         Api api = injector.getInstance(Api.class);
 
-
         switch (type) {
             case "tls":
-                this.modelLoader = new ModelLoader(api, Config.TLSMODELFILEPATH);
+                this.modelLoader = new ModelLoader(
+                    api,
+                    Config.TLSMODELFILEPATH
+                );
 
-                if ("client".equalsIgnoreCase(mode)) {
-                    this.systemUnderTest = new TLSSystemUnderTest("client");
-                    this.oppositeTester = new TLSAttackerServerRunner();
-                } else if ("server".equalsIgnoreCase(mode)) {
-                    this.systemUnderTest = new TLSSystemUnderTest("server");
-                    this.oppositeTester = new TLSAttackerClientRunner();
-                } else {
-                    throw new IllegalArgumentException("Unknown SUT mode: " + mode);
-                }
-                // this.systemUnderTest = new TLSSystemUnderTest();
+                this.systemUnderTest = new TLSAttackerSUTServer();
+                this.fakeClient = new TLSAttackerFakeClient();
+
                 break;
-
             //Simply to show the flexibility and adaptation of the code
             case "tlsTesting":
-                this.modelLoader = new ModelLoader(api, Config.TLSMODELFORTESTINGFILEPATH);
+                this.modelLoader = new ModelLoader(
+                    api,
+                    Config.TLSMODELFORTESTINGFILEPATH
+                );
                 //this.systemUnderTest = new SystemUnderTest();
                 break;
             default:
@@ -62,28 +60,37 @@ public class TestExaminer {
         }
     }
 
-    public void runTest(){
-        System.out.println("Stargin TLS Test (mode: " + mode + ")");
+    public void runTest() {
+        System.out.println("-- Starting TLS Test --");
 
         // Load model
         loadModel();
 
         // Launch real SUT (System Under Test)
         if (systemUnderTest != null) {
+            System.out.println("Starting SUT ...");
+            systemUnderTest.createSUT();
             systemUnderTest.startSUT();
         }
 
-        // Create and execute SUT (client or server)
-        if (oppositeTester != null){
-            oppositeTester.createSUT();
-            if ("server".equalsIgnoreCase(mode)){
-                oppositeTester.startSUT();
-                oppositeTester.executeSUTOperation();
-            }
+        // Create and execute the fake client with tls attacker as a client
+        if (fakeClient != null) {
+            System.out.println("Starting client with tls attacker...");
+            fakeClient.createSUT();
         }
 
         // Results comparison
-        compareResults();
+        // compareResults();
+
+        boolean isValid = modelLoader.validateServerHelloFromSUT("src/main/resources/data/SUTServerHello.yaml");
+
+        if (isValid) {
+            System.out.println("ServerHello is valid according to the model.");
+        } else {
+            System.out.println("ServerHello is invalid according to the model.");
+        }
+
+        
     }
 
     public void loadModel() {
@@ -92,42 +99,60 @@ public class TestExaminer {
         this.modelLoader.modelInformation();
     }
 
-    public void createSUT() {
-        systemUnderTest.startSUT();
-    }
-    public void createSUTForServerHello(){
+    // public void createSUT() {
+    //     systemUnderTest.startSUT();
+    // }
 
-    }
+    public void createSUTForServerHello() {}
 
-    public void executeSUTOperation() {
+    public void executeSUTOperation() {}
 
-    }
+    //    KEEPING FOR FURTHER IMPLEMENTATIONS
+    //    public void executeModelOperation() {
+    //        this.modelLoader.executeSpecificTrace();
+    //    }
 
-//    KEEPING FOR FURTHER IMPLEMENTATIONS
-//    public void executeModelOperation() {
-//        this.modelLoader.executeSpecificTrace();
-//    }
-
-    public void testServerHello(){
+    public void testServerHello() {
         this.modelLoader.generateClientAndServerHello();
     }
 
     public void compareResults() {
-        System.out.println("Comparing YAML resultas");
+        System.out.println("Comparing YAML result");
         boolean match;
 
-        switch (mode){
+        switch (mode) {
             case "client":
-                match = AbstractInformationComparator.compareAbstractYaml( InformationConvertertoAbstract.retreiveYamlInformation("src/main/ressources/data/SUTServerHello.yaml"), InformationConvertertoAbstract.retreiveYamlInformation("src/main/ressources/data/ModelServerHello.yaml"), "");
+                match = AbstractInformationComparator.compareAbstractYaml(
+                    InformationConvertertoAbstract.retreiveYamlInformation(
+                        "src/main/resources/data/SUTServerHello.yaml"
+                    ),
+                    InformationConvertertoAbstract.retreiveYamlInformation(
+                        "src/main/resources/data/ModelServerHello.yaml"
+                    ),
+                    ""
+                );
                 break;
             case "server":
-                match = AbstractInformationComparator.compareAbstractYaml( InformationConvertertoAbstract.retreiveYamlInformation("src/main/ressources/data/SUTClientHello.yaml"), InformationConvertertoAbstract.retreiveYamlInformation("src/main/ressources/data/ModelClientHello.yaml"), "");
+                match = AbstractInformationComparator.compareAbstractYaml(
+                    InformationConvertertoAbstract.retreiveYamlInformation(
+                        "src/main/resources/data/SUTClientHello.yaml"
+                    ),
+                    InformationConvertertoAbstract.retreiveYamlInformation(
+                        "src/main/resources/data/ModelClientHello.yaml"
+                    ),
+                    ""
+                );
                 break;
             default:
                 throw new IllegalArgumentException("Unknown SUT mode: " + mode);
         }
 
         System.out.println(match ? "Match found!" : "No match found!");
-
     }
+
+
+    
+
+
+
 }
